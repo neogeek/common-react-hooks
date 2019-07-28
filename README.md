7dc51cd3512c335b58d3ba25e103d4c155d9fd5f
neogeek <neogeek0101@gmail.com>
2019-07-28 17:59:21 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# @neogeek/common-react-hooks
+
+## Documentation
+
+### `useUniqueId`
+
+```javascript
+const id = useUniqueId();
+```
+
+```javascript
+const id = useUniqueId('form-field');
+```
---
+++ hooks/useUniqueId.js
@@ -0,0 +1,11 @@
+var ids = new Map();
+
+const useUniqueId = (prefix = 'id') => {
+    const num = ids.get(prefix) || 0;
+
+    ids.set(prefix, num + 1);
+
+    return `${prefix}-${num}`;
+};
+
+export default useUniqueId;
