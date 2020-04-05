0ca7c961100a191f672e79f4f8b3d358a56508ef
Scott Doxey <neogeek0101@gmail.com>
2020-04-05 09:49:46 -0400

    Formatted files.

--- hooks/useFetchJSON.js
+++ hooks/useFetchJSON.js
@@ -1,68 +1,49 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 const HTTP_CODE_SUCCESS = 200;
 const HTTP_CODE_REDIRECTION = 300;
 
 const useFetchJSON = (url, headers = {}, defaultValue = {}) => {
-
-    const [
-        data,
-        setData
-    ] = useState(defaultValue);
+    const [data, setData] = useState(defaultValue);
 
     let hasCanceled = false;
 
     useEffect(() => {
-
         fetch(url, headers)
-            .then(response => {
-
+            .then((response) => {
                 if (hasCanceled) {
-
                     return Promise.reject(new Error('Request canceled'));
-
                 }
 
                 return Promise.resolve(response);
-
             })
-            .then(response => {
-
+            .then((response) => {
                 if (
                     response.status >= HTTP_CODE_SUCCESS &&
-          response.status < HTTP_CODE_REDIRECTION
+                    response.status < HTTP_CODE_REDIRECTION
                 ) {
-
                     return Promise.resolve(response);
-
                 }
 
                 const error = new Error(response.statusText || response.status);
                 error.response = response;
 
                 return Promise.reject(error);
-
             })
-            .then(response => response.json())
+            .then((response) => response.json())
             .then(setData)
-            .catch(err => {
-
+            .catch((err) => {
                 setData(defaultValue);
 
                 throw err;
-
             });
 
         return () => {
-
             hasCanceled = true;
-
         };
-
     }, [url]);
 
     return [data];
-
 };
 
 export default useFetchJSON;
--- hooks/useUniqueId.js
+++ hooks/useUniqueId.js
@@ -1,13 +1,11 @@
 const ids = new Map();
 
 const useUniqueId = (prefix = 'id') => {
-
     const num = ids.get(prefix) || 0;
 
     ids.set(prefix, num + 1);
 
     return `${prefix}-${num}`;
-
 };
 
 export default useUniqueId;
