ea3695c108abbf5b1fa36aafd42a95e252741206
Scott Doxey <neogeek0101@gmail.com>
2020-07-09 19:57:57 -0400

    Fixed an issue where value would not be updated when location was updated.

--- hooks/useSearchParam.js
+++ hooks/useSearchParam.js
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react';
 
 import qs from 'qs';
 
+export const getSearchParams = () =>
+    qs.parse(window.location.search, {
+        ignoreQueryPrefix: true
+    });
+
 export const useSearchParam = (name, historyMethod = 'replaceState') => {
-    const [value, setValue] = useState(
-        qs.parse(window.location.search, { ignoreQueryPrefix: true })[name] ||
-            ''
-    );
+    const [value, setValue] = useState(getSearchParams()[name] || '');
 
     useEffect(() => {
-        const params = qs.parse(window.location.search, {
-            ignoreQueryPrefix: true
-        });
+        const params = getSearchParams();
+
+        if (params[name] === value) {
+            return;
+        }
 
         window.history[historyMethod](
             {},
@@ -22,6 +26,10 @@ export const useSearchParam = (name, historyMethod = 'replaceState') => {
             )}${window.location.hash}`
         );
     }, [value]);
+
+    useEffect(() => {
+        setValue(getSearchParams()[name] || '');
+    }, [window.location.search]);
 
     return [value, setValue];
 };
