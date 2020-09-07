1a4181386287fcb86881af9a0a36d961051687f6
Scott Doxey <neogeek0101@gmail.com>
2020-09-07 00:59:19 -0400

    Added tests.

--- __tests__/useFetchJSON.js
+++ __tests__/useFetchJSON.js
@@ -8,11 +8,17 @@ import { render, screen } from '@testing-library/react';
 
 import { useFetchJSON } from '../hooks/useFetchJSON';
 
+const originalFetch = global.fetch;
+
 beforeAll(() => {
     global.fetch = () => {};
 });
 
-test('check data is updated via fetch', async () => {
+afterAll(() => {
+    global.fetch = originalFetch;
+});
+
+test('check data is retrieved via fetch', async () => {
     const ExampleFunction = () => {
         const [data, update] = useFetchJSON('http://localhost:8000/api', null, {
             default: 'value'
@@ -37,4 +43,51 @@ test('check data is updated via fetch', async () => {
     });
 
     expect(screen.getByText('{"default":"value"}')).toBeInTheDocument();
+});
+
+describe('error handling', () => {
+    const originalConsoleError = console.error;
+
+    const errorOuput = [];
+
+    beforeEach(() => {
+        console.error = message => errorOuput.push(message);
+    });
+
+    afterEach(() => {
+        console.error = originalConsoleError;
+    });
+
+    test('check if request is rejected with invalid content type', async () => {
+        const ExampleFunction = () => {
+            const [data, update] = useFetchJSON(
+                'http://localhost:8000/api',
+                null,
+                {
+                    default: 'value'
+                }
+            );
+
+            return (
+                <div>
+                    <span>{JSON.stringify(data)}</span>
+                </div>
+            );
+        };
+
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+            Promise.resolve({
+                headers: { get: () => 'text/plain' },
+                json: () => Promise.resolve('ok')
+            })
+        );
+
+        await act(async () => {
+            render(<ExampleFunction />);
+        });
+
+        expect(errorOuput[errorOuput.length - 1]).toEqual(
+            'Error: Invalid content type.'
+        );
+    });
 });
