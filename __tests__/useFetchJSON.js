f43ebac7ed05440aa8208aaeacfe5e0241e2a3e2
Scott Doxey <neogeek0101@gmail.com>
2020-09-06 22:08:55 -0400

    Added test for useFetchJSON.

---
+++ __tests__/useFetchJSON.js
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom';
+
+import React, { useRef } from 'react';
+
+import { act } from 'react-dom/test-utils';
+
+import { render, screen } from '@testing-library/react';
+
+import { useFetchJSON } from '../hooks/useFetchJSON';
+
+beforeAll(() => {
+    global.fetch = () => {};
+});
+
+test('check data is updated via fetch', async () => {
+    const ExampleFunction = () => {
+        const [data, update] = useFetchJSON('http://localhost:8000/api', null, {
+            default: 'value'
+        });
+
+        return (
+            <div>
+                <span>{JSON.stringify(data)}</span>
+            </div>
+        );
+    };
+
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+        Promise.resolve({
+            headers: { get: () => 'application/json' },
+            json: () => Promise.resolve({ default: 'value' })
+        })
+    );
+
+    await act(async () => {
+        render(<ExampleFunction />);
+    });
+
+    expect(screen.getByText('{"default":"value"}')).toBeInTheDocument();
+});
