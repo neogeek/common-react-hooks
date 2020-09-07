756d34a1cc7649920ed955b633c8221a9ba71a14
Scott Doxey <neogeek0101@gmail.com>
2020-09-06 20:49:39 -0400

    Added usePrevious method.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { useFetchJSON } from './hooks/useFetchJSON';
 export { useLocalStorage } from './hooks/useLocalStorage';
+export { usePrevious } from './hooks/usePrevious';
 export { useSearchParam } from './hooks/useSearchParam';
 export { useUniqueId } from './hooks/useUniqueId';
