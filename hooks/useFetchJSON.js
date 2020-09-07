2d5f7a2ada8c40b618fed94582360d415ec13401
Scott Doxey <neogeek0101@gmail.com>
2020-09-07 01:37:31 -0400

    Simplified useFetchJSON.

--- hooks/useFetchJSON.js
+++ hooks/useFetchJSON.js
@@ -5,7 +5,6 @@ const HTTP_CODE_REDIRECTION = 300;
 
 export const useFetchJSON = (url, headers = {}, defaultValue = {}) => {
     const [data, setData] = useState(defaultValue);
-
     const [forceReload, setForceReload] = useState();
 
     useEffect(() => {
@@ -16,9 +15,6 @@ export const useFetchJSON = (url, headers = {}, defaultValue = {}) => {
                     return Promise.reject(new Error('Request canceled'));
                 }
 
-                return Promise.resolve(response);
-            })
-            .then(response => {
                 if (
                     response.status < HTTP_CODE_SUCCESS ||
                     response.status > HTTP_CODE_REDIRECTION
