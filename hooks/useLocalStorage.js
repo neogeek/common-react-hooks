36550e91b755e7f4a0c5c4fcd158b374febfdefa
Scott Doxey <neogeek0101@gmail.com>
2020-09-08 01:44:40 -0400

    Better handling of null and undefined values in useLocalStorage hook.

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export const useLocalStorage = (key, defaultValue = '') => {
+export const useLocalStorage = (key, defaultValue = null) => {
     const [localValue, setLocalValue] = useState(() => {
         const value =
             localStorage.getItem(key) ||
@@ -16,7 +16,11 @@ export const useLocalStorage = (key, defaultValue = '') => {
     });
 
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(localValue));
+        if (localValue === null || localValue === undefined) {
+            localStorage.removeItem(key);
+        } else {
+            localStorage.setItem(key, JSON.stringify(localValue));
+        }
     }, [key, localValue]);
 
     return [localValue, setLocalValue];
