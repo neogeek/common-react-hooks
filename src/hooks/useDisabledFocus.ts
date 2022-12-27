2980a86e6af5cd9c219cb1720805f2470d1e01bb
Scott Doxey <neogeek0101@gmail.com>
2022-12-27 16:28:52 -0500

    [feat] Added useDisabledFocus hook. (#4)
    * Added useDisabledFocus hook.
    
    Added test.
    
    * Blur object when it's marked as disabled.
    
    Updated tests.
    
    * Removed unneeded statement.

---
+++ src/hooks/useDisabledFocus.ts
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+export default function useDisabledFocus<T>(
+  ref: React.RefObject<T>,
+  disabled: boolean
+) {
+  const [focused, setFocused] = useState(false);
+
+  useEffect(() => {
+    if (
+      !(
+        ref.current instanceof HTMLInputElement ||
+        ref.current instanceof HTMLTextAreaElement ||
+        ref.current instanceof HTMLSelectElement ||
+        ref.current instanceof HTMLButtonElement
+      )
+    )
+      return;
+
+    if (disabled && ref.current === document.activeElement) {
+      ref.current.blur();
+
+      setFocused(true);
+    }
+
+    if (disabled) {
+      ref.current.setAttribute('disabled', 'disabled');
+    } else {
+      ref.current.removeAttribute('disabled');
+    }
+
+    if (!disabled && focused) {
+      ref.current.focus();
+
+      setFocused(false);
+    }
+  }, [disabled, focused, ref]);
+}
